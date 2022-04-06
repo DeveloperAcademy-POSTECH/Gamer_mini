0cdf6cc7c28dd1d34d89e3832332aa7e959f2d9e
Lee Myeonghwan <leemyeonghwan@Leeui-MacBookPro.local>
2022-04-06 09:23:33 +0900

    create project

---
+++ donggle/donggle/donggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  donggleApp.swift
+//  donggle
+//
+//  Created by Lee Myeonghwan on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct donggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
