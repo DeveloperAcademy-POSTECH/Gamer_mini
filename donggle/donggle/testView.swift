4a016bcb766ee519ab8caca660859e55de445c77
Lee Myeonghwan <leemyeonghwan@Leeui-MacBookPro.local>
2022-04-07 17:34:50 +0900

    test conflict

---
+++ donggle/donggle/testView.swift
@@ -0,0 +1,20 @@
+//
+//  testView.swift
+//  donggle
+//
+//  Created by Lee Myeonghwan on 2022/04/07.
+//
+
+import SwiftUI
+
+struct testView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct testView_Previews: PreviewProvider {
+    static var previews: some View {
+        testView()
+    }
+}
