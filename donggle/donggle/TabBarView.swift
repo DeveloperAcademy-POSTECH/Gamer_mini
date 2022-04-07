dc7fbc06b1a8861c5dd8d5d45569a5c4c1ac5bc3
이가은 <82457928+rriver2@users.noreply.github.com>
2022-04-07 12:57:44 +0900

    add CalenderView

---
+++ donggle/donggle/TabBarView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  rewardCalender
+//
+//  Created by 이가은 on 2022/04/06.
+//
+
+import SwiftUI
+
+
+struct TabBarView: View{
+    @State private var selection = 3
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor.white
+    }
+    
+    var body: some View{
+        
+        TabView(selection:$selection){
+            
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }.tag(1)
+
+            ReportView()
+                .tabItem {
+                    Image(systemName: "doc.text.magnifyingglass")
+                    Text("Report")
+                }.tag(2)
+            
+            CalendarView()
+                .tabItem {
+                    Image(systemName: "square.and.pencil")
+                    Text("Calendar")
+                }.tag(3)
+            
+            SettingView()
+                .tabItem {
+                    Image(systemName: "gearshape")
+                    Text("Setting")
+                }.tag(4)
+            
+        }.accentColor(.green)
+        
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
+
