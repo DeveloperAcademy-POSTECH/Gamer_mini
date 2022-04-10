2aa2e20e7187d3bb9180099b2a44198dc02b8550
Goban <gobanest@gmail.com>
2022-04-10 10:23:13 +0900

    Update ContentView.swift
    사라진  contentView 복구

--- donggle/donggle/Views/ContentView.swift
+++ donggle/donggle/Views/ContentView.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 import Foundation
 
+struct ContentView: View {
+    var body: some View {
+            VStack {
+                Text("Hello world")
+            }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        TabBarView()
+        ContentView()
     }
 }
