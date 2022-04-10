30e5fb00ead21660ad74490191c3bdec4a8b79cd
Kim Hwiwon <kimhwiwon@Kimui-MacBookPro.local>
2022-04-11 01:00:18 +0900

    #47 [Feat] 타임라인 스트레스 카드 UserDefaults 데이터 사용하도록 구현

--- donggle/donggle/Views/TimelineView.swift
+++ donggle/donggle/Views/TimelineView.swift
@@ -25,24 +25,7 @@ struct TimelineView: View {
         var type: Int //스트레스(0) or 보상(1) 데이터 체크
         var icon: String //보상 카테고리 아이콘
     }
-    
-    struct Stress2 {
-        var id : Int
-        var index : Int
-        var content : String
-        var date : Date
-        var category : String
-        var rewardKey : UUID?
-    }
-    
-    @State private var stressInfo : [Stress2] = [
-        Stress2(id: 0, index: 13, content: "사과 스트레스", date: Date(), category: "🍎", rewardKey: nil),
-        Stress2(id: 1, index: 23, content: "불꽃 스트레스", date: Date(), category: "🔥", rewardKey: nil),
-        Stress2(id: 2, index: 37, content: "일이 너무 많고 어렵다. 맨날 야근을 해야하는데 잠이 너무 부족하고 짜증난다. 직장 사람들과는 친하지도 않아서 자리가 불편하다.", date: Date(), category: "직장", rewardKey: nil),
-        Stress2(id: 3, index: 47, content: "이사를 가기 너무 귀찮다", date: Date(), category: "일상", rewardKey: nil),
-        Stress2(id: 4, index: 17, content: "잠이 너무 부족하다.. 잠이 필요하다. 내일도 아침에 일찍 일어나야하는데, 벌써 새벽 두 시가 넘었다. 주말에 잠을 몰아서 자야할 것 같다. 요즘 잠을 자지 못해서 머리가 잘 안 돌아가는 것 같고, 건강도 안 좋아지는 것 같다.", date: Date(), category: "수면", rewardKey: nil)
-    
-    ]
+
     
     @State private var rewardInfo : [Reward2] = [
         Reward2(id : 0, title : "맥주 한 잔!", content : "오늘 요정들과 함께 뿌링클 치킨 사서 치맥하기!", date : Date(), category : "음주", isEffective : nil, stressKey : nil, type: 1, icon : "🍺" ),
@@ -52,13 +35,15 @@ struct TimelineView: View {
         Reward2(id : 4, title : "잠자기", content : "메로나 먹고싶어", date : Date(), category : "수면", isEffective : nil, stressKey : 1, type: 1, icon : "💤" ),
         Reward2(id : 5, title : "흐느적거리기", content : "메로나 먹고싶어", date : Date(), category : "휴식", isEffective : nil, stressKey : 1, type: 1, icon : "🐙" ),
         Reward2(id : 6, title : "꿈틀거리기", content : "메로나 먹고싶어", date : Date(), category : "휴식", isEffective : nil, stressKey : 1, type: 1, icon : "🪱" )]
-
-
     
+    let stressSet = UserDefaults.stressArray ?? []
+
     
     var body: some View {
         
+        
                 VStack{
+                    
                     HStack{
                         
                         Button(action: {
@@ -90,8 +75,8 @@ struct TimelineView: View {
                     if (selectedView == 1){ //전체
                         ScrollView(showsIndicators: false) {
                             LazyVGrid(columns: [GridItem()], alignment: .center, spacing: 5){
-                                ForEach(stressInfo, id: \.self.id) { stress in
-                                    StressCard(stressIndex:stress.index, stressName: stress.category, stressContent: stress.content)
+                                ForEach(stressSet, id: \.self.id) { stress in
+                                    StressCard(stressIndex:stress.index, stressContent: stress.content, stressCateList: getStressCateList(stressCategory : stress.category))
                                 }
                                 ForEach(rewardInfo, id: \.self.id) { reward in
                                     GiftCard(giftIcon: reward.icon, giftName: reward.category, giftTitle: reward.title, giftContent: reward.content)
@@ -103,8 +88,8 @@ struct TimelineView: View {
                     }else if (selectedView == 2){ //스트레스
                         ScrollView(showsIndicators: false) {
                             LazyVGrid(columns: [GridItem()], alignment: .center, spacing: 5){
-                                ForEach(stressInfo, id: \.self.id) { stress in
-                                    StressCard(stressIndex:stress.index, stressName: stress.category, stressContent: stress.content)
+                                ForEach(stressSet, id: \.self.id) { stress in
+                                    StressCard(stressIndex:stress.index, stressContent: stress.content, stressCateList: getStressCateList(stressCategory : stress.category))
                                 }
                             }
                         }
@@ -178,8 +163,9 @@ struct GiftCard : View {
 
 struct StressCard : View {
     var stressIndex: Int
-    var stressName: String
     var stressContent: String
+    var stressCateList: String
+        
     var body: some View {
         VStack(spacing: 5.0){
             Text("7 목")
@@ -201,19 +187,25 @@ struct StressCard : View {
                     .stroke(lineWidth: 0)
                 )
                 
-//                Spacer()
                 VStack(alignment: .leading, spacing: 3.0){
-                    HStack{
-                        Text("스트레스")
-                            .padding(.horizontal, 5.0)
-                            .foregroundColor(.white)
-                        .background(RoundedRectangle(cornerRadius: 15)
-                            .foregroundColor(.gray)
-                        )
-                        Text(stressName)
+                    HStack(alignment: .top){ //카테고리 두 줄 이상일 때 위쪽으로 정렬되도록
+                        
+                            Text("스트레스")
+                                .padding(.horizontal, 5.0)
+                                .foregroundColor(.white)
+                            .background(RoundedRectangle(cornerRadius: 15)
+                                .foregroundColor(.gray)
+                            )
+                            
+//                        ForEach(stressCategory, id:\.self){ category in
+//                            Text(category)
+//                            stressCateList.append(category)
+                            
+//                        }
+                        Text(stressCateList)
                         Spacer()
                     }
-//                                Divider()
+
                     Text(stressContent)
                         .font(.body)
                     Spacer()
@@ -230,6 +222,20 @@ struct StressCard : View {
 
 }
 
+//여러개의 카테고리가 ","로 구분되어 나열된 string 만들기
+func getStressCateList(stressCategory : [String]) -> String {
+    
+    var stressCateArr : [String] = []
+    var stressCateList : String
+    
+    for category in stressCategory{
+        stressCateArr.append(category)
+    }
+    
+    stressCateList = stressCateArr.joined(separator: ", ")
+    
+    return stressCateList
+}
 
 
 struct TimelineView_Previews: PreviewProvider {
